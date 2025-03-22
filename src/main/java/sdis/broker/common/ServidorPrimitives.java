e440c07282521304ab961b2961e1a947f3db723c
dandelp <daniel.pozo23@estudiantes.uva.es>
2025-03-22 16:56:42 +0100

    feat: interfaces for validate who emits the primitive

---
+++ src/main/java/sdis/broker/common/ServidorPrimitives.java
@@ -0,0 +1,23 @@
+package sdis.broker.common;
+
+import java.util.Set;
+
+public interface ServidorPrimitives {
+    public static final Set<Primitiva> PRIMITIVES_0_ARGS =
+            Set.of(
+                    Primitiva.INFO,
+                    Primitiva.ADDED,
+                    Primitiva.DELETED,
+                    Primitiva.EMPTY
+            );
+
+    public static final Set<Primitiva> PRIMITIVES_1_ARGS =
+            Set.of(
+                    Primitiva.XAUTH,
+                    Primitiva.MSG,
+                    Primitiva.BARCODE,
+                    Primitiva.STATE,
+                    Primitiva.NOAUTH,
+                    Primitiva.ERROR
+            );
+}
